6b11fbf0245f39c31cb81359f8463e0a0d1bdba2
Tiger136500 <749031134@11.com>
2019-04-24 11:12:22 +0800

    test

---
+++ FirstWebApp/www/app.py
@@ -0,0 +1,25 @@
+import aiomysql
+from aiohttp import web
+from datetime import datetime
+import logging
+
+logging.basicConfig(level=logging.INFO)
+import asyncio, os, json, time
+
+
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html')
+
+
+@asyncio.coroutine0
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server start at http://127.0.0.1:9000')
+    return srv
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
