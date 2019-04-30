9ec6291f7a99d20241030dceb1ada8b8930c58a0
Tiger136500 <749031134@11.com>
2019-04-30 15:52:47 +0800

    login and register

--- FirstWebApp/www/handlers.py
+++ FirstWebApp/www/handlers.py
@@ -130,7 +130,7 @@ async def authenticate(*, email, passwd):
     users = await  User.findAll('email=?', [email])
     if len(users) == 0:
         raise APIValueError('email', 'Email not exist.')
-    user = user[0]
+    user = users[0]
     # check password
     sha1 = hashlib.sha1()
     sha1.update(user.id.encode('utf-8'))
@@ -140,7 +140,6 @@ async def authenticate(*, email, passwd):
         raise APIValueError('passwd', 'Invalid password.')
     # authenticate ok set cookie:
     r = web.Response()
-    0
     r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
     user.passwd = '******'
     r.content_type = 'application/json'
@@ -284,9 +283,9 @@ async def api_register_user(*, email, name, passwd):
         raise APIError('register:failed', 'email', 'Email is already in use.')
     uid = next_id()
     sha1_passwd = '%s:%s' % (uid, passwd)
-    user = User(id=uid, name=name.strip(), email=email, passwd=hashlib.sha1(sha1_passwd.encode('utf-8')))
-    await  user.save()
-    # make a session cookie
+    user = User(id=uid, name=name.strip(), email=email, passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(), image='http://www.gravatar.com/avatar/%s?d=mm&s=120' % hashlib.md5(email.encode('utf-8')).hexdigest())
+    await user.save()
+    # make session cookie:
     r = web.Response()
     r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
     user.passwd = '******'
--- FirstWebApp/www/orm.py
+++ FirstWebApp/www/orm.py
@@ -1,16 +1,15 @@
-import asyncio, logging
-import aiomysql
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-__author__ = '谢云琪Copy'
+__author__ = 'David Yang'
 
+import asyncio, logging, aiomysql
 
 def log(sql, args=()):
-    logging.info('SQL：%s' % sql)
+    logging.info('SQL: %s' % sql)
 
-
-# 创建数据库连接池
 async def create_pool(loop, **kw):
-    logging.info("创建数据库连接池")
+    logging.info('create database connection pool...')
     global __pool
     __pool = await aiomysql.create_pool(
         host=kw.get('host', 'localhost'),
@@ -25,8 +24,6 @@ async def create_pool(loop, **kw):
         loop=loop
     )
 
-
-# Select查询语句
 async def select(sql, args, size=None):
     log(sql, args)
     global __pool
@@ -38,13 +35,13 @@ async def select(sql, args, size=None):
         else:
             rs = await cur.fetchall()
         await cur.close()
-        logging.info('rows return:%s' % len(rs))
+        logging.info('rows returned: %s' % len(rs))
         return rs
 
-
-# insert update  delete通用
 async def execute(sql, args):
     log(sql)
+    for z in args:
+        print(z)
     with (await __pool) as conn:
         try:
             cur = await conn.cursor()
@@ -55,103 +52,48 @@ async def execute(sql, args):
             raise
         return affected
 
-
-# 不知道干啥瞎几把打的函数
-def create_args_string(num):
-    L = []
-    for n in range(num):
-        L.append("?")
-
-    return ', '.join(L)
-
-
-# 不知道干啥瞎几把打的基类
-class Field(object):
-    def __init__(self, name, column_type, primary_key, default):
-        self.name = name
-        self.column_type = column_type
-        self.primary_key = primary_key
-        self.default = default
-
-    def __str__(self):
-        return '<%s,%s:%s>' % (self.__class__.__name__, self.column_type, self.name)
-
-
-# 不知道干啥瞎几把打的String类
-# ddl 理论上应该是映射Mysql数据库中的varchar类型数据  以此类推
-class StringField(Field):
-
-    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
-        super().__init__(name, ddl, primary_key, default)
-
-
-# 不知道干啥瞎几把打的boolean类
-class BooleanField(Field):
-
-    def __init__(self, name=None, default=False):
-        super().__init__(name, 'boolean', False, default)
-
-
-# 不知道干啥瞎几把打的Integer类
-class IntegerField(Field):
-
-    def __init__(self, name=None, primary_key=False, default=0):
-        super().__init__(name, 'bigint', primary_key, default)
-
-
-# 不知道干啥瞎几把打的Float类
-class FloatField(Field):
-
-    def __init__(self, name=None, primary_key=False, default=0.0):
-        super().__init__(name, 'real', primary_key, default)
-
-
-# 不知道干啥瞎几把打的Text类
-class TextField(Field):
-
-    def __init__(self, name=None, default=None):
-        super().__init__(name, 'text', False, default)
-
-
 class ModelMetaclass(type):
-    def __new__(cls, name, bases, attrs):
-        if name == 'Model':
-            return type.__new__(cls, name, bases, attrs)
 
+    def __new__(cls, name, bases, attrs):
+        # 排除Model类本身:
+        if name=='Model':
+            return type.__new__(cls, name, bases, attrs)
+        # 获取table名称:
         tableName = attrs.get('__table__', None) or name
-        logging.info('found model:%s (table: %s)' % (name, tableName))
+        logging.info('found model: %s (table: %s)' % (name, tableName))
+        # 获取所有的Field和主键名:
         mappings = dict()
         fields = []
         primaryKey = None
         for k, v in attrs.items():
             if isinstance(v, Field):
-                logging.info('found mapping:%s ==> %s' % (k, v))
+                logging.info('  found mapping: %s ==> %s' % (k, v))
                 mappings[k] = v
                 if v.primary_key:
+                    # 找到主键:
                     if primaryKey:
                         raise RuntimeError('Duplicate primary key for field: %s' % k)
                     primaryKey = k
                 else:
                     fields.append(k)
         if not primaryKey:
-            raise RuntimeError('Prinmary key not found.')
+            raise RuntimeError('Primary key not found.')
         for k in mappings.keys():
             attrs.pop(k)
         escaped_fields = list(map(lambda f: '`%s`' % f, fields))
-        attrs['__mappings__'] = mappings  # 保存属性和列的映射关系
-        attrs['__table__'] = tableName  # 表名
-        attrs['__primary_key__'] = primaryKey  # 主键属性名
-        attrs['__fields__'] = fields  # 除开主键外其他属性名
-        attrs['__select__'] = 'select `%s`,%s from  `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
-        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (
-            tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
-        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (
-            tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+        attrs['__mappings__'] = mappings # 保存属性和列的映射关系
+        attrs['__table__'] = tableName
+        attrs['__primary_key__'] = primaryKey # 主键属性名
+        attrs['__fields__'] = fields # 除主键外的属性名
+        # 构造默认的SELECT, INSERT, UPDATE和DELETE语句:
+        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
         attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
         return type.__new__(cls, name, bases, attrs)
 
-
 class Model(dict, metaclass=ModelMetaclass):
+
     def __init__(self, **kw):
         super(Model, self).__init__(**kw)
 
@@ -159,7 +101,7 @@ class Model(dict, metaclass=ModelMetaclass):
         try:
             return self[key]
         except KeyError:
-            raise AttributeError(r"'Model' object has no attrbute '%s'" % key)
+            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
 
     def __setattr__(self, key, value):
         self[key] = value
@@ -179,7 +121,7 @@ class Model(dict, metaclass=ModelMetaclass):
 
     @classmethod
     async def findAll(cls, where=None, args=None, **kw):
-        'find objects by where clause'
+        ## find objects by where clause
         sql = [cls.__select__]
         if where:
             sql.append('where')
@@ -197,16 +139,16 @@ class Model(dict, metaclass=ModelMetaclass):
                 sql.append('?')
                 args.append(limit)
             elif isinstance(limit, tuple) and len(limit) == 2:
-                sql.append('?,?')
+                sql.append('?, ?')
                 args.extend(limit)
             else:
-                raise ValueError('Invalid limit value:%s' % str(limit))
+                raise ValueError('Invalid limit value: %s' % str(limit))
         rs = await select(' '.join(sql), args)
         return [cls(**r) for r in rs]
 
     @classmethod
     async def findNumber(cls, selectField, where=None, args=None):
-        # 'find Number bu select and where'
+        ## find number by select and where
         sql = ['select %s _num_ from `%s`' % (selectField, cls.__table__)]
         if where:
             sql.append('where')
@@ -218,9 +160,9 @@ class Model(dict, metaclass=ModelMetaclass):
 
     @classmethod
     async def find(cls, pk):
-        'find  object by primary key '
-        rs = await  select('%s where  `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
-        if len(rs)==0:
+        ## find object by primary key
+        rs = await select('%s where `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
+        if len(rs) == 0:
             return None
         return cls(**rs[0])
 
@@ -229,7 +171,7 @@ class Model(dict, metaclass=ModelMetaclass):
         args.append(self.getValueOrDefault(self.__primary_key__))
         rows = await execute(self.__insert__, args)
         if rows != 1:
-            logging.warn('failed to insert record:affected rows:%s' % rows)
+            logging.warn('failed to insert record: affected rows: %s' % rows)
 
     async def update(self):
         args = list(map(self.getValue, self.__fields__))
@@ -243,3 +185,47 @@ class Model(dict, metaclass=ModelMetaclass):
         rows = await execute(self.__delete__, args)
         if rows != 1:
             logging.warn('failed to remove by primary key: affected rows: %s' % rows)
+
+
+class Field(object):
+
+    def __init__(self, name, column_type, primary_key, default):
+        self.name = name
+        self.column_type = column_type
+        self.primary_key = primary_key
+        self.default = default
+
+    def __str__(self):
+        return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
+
+class StringField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+        super().__init__(name, ddl, primary_key, default)
+
+class BooleanField(Field):
+
+    def __init__(self, name=None, default=False):
+        super().__init__(name, 'boolean', False, default)
+
+class IntegerField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=0):
+        super().__init__(name, 'bigint', primary_key, default)
+
+class FloatField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=0.0):
+        super().__init__(name, 'real', primary_key, default)
+
+class TextField(Field):
+
+    def __init__(self, name=None, default=None):
+        super().__init__(name, 'text', False, default)
+
+
+def create_args_string(num):
+    L = []
+    for n in range(num):
+        L.append('?')
+    return ', '.join(L)
